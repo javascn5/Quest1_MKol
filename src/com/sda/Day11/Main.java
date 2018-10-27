d22315da8fcb8fcbc5db62c176dc3742494db148
Kolachow <kolas3000@gmail.com>
2018-10-27 11:41:05 +0200

    Zad. 9 - palindrom

--- src/com/sda/Day11/Main.java
+++ src/com/sda/Day11/Main.java
@@ -72,13 +72,22 @@ public class Main {
 //        list.showNotEven();
 
 //todo Zad. 8
+//        String word;
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Podaj słowo: ");
+//        word = scanner.nextLine();
+//        String halfWord = word.substring(0, word.length()/2);
+//        System.out.println("Slowo wyswietlone w polowie: " + halfWord);
+//        System.out.println("Dlugosc slowa: " + word.length());
+
+//todo Zad. 9
         String word;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Podaj słowo: ");
+        System.out.print("Podaj slowo: ");
         word = scanner.nextLine();
-        String halfWord = word.substring(0, word.length()/2);
-        System.out.println("Slowo wyswietlone w polowie: " + halfWord);
-        System.out.println("Dlugosc slowa: " + word.length());
 
+        Palindrom palindrom = new Palindrom();
+
+        System.out.println("Czy slowo jest palindromem: " + palindrom.palindrom(word));
     }
 }
