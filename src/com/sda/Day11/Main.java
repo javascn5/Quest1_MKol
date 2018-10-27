489f01732f7e2145253d0821827ff682194a1300
Kolachow <kolas3000@gmail.com>
2018-10-27 09:57:18 +0200

    Zad. 3

---
+++ src/com/sda/Day11/Main.java
@@ -0,0 +1,33 @@
+package com.sda.Day11;
+
+import java.util.Scanner;
+public class Main {
+
+    public static final int NUMBERS = 5;
+
+    public static void main(String[] args) {
+
+        int suma = 0;
+        int[] tab = new int[NUMBERS];
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < NUMBERS; i++) {
+            System.out.print("Podaj liczbe calkowita: ");
+            tab[i] = scanner.nextInt();
+            if (tab[i] % 2 == 0) {
+                suma += tab[i];
+            } else {
+                suma -= tab[i];
+            }
+
+        }
+
+        System.out.println("Suma: " + suma);
+        System.out.print("Podane liczby: ");
+        for (int liczby: tab
+             ) {
+            System.out.print(liczby + "\t");
+        }
+    }
+}
